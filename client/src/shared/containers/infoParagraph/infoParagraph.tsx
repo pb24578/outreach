18b619491941b26105c4c8a612b132c29cd356e6
fronrich <fronrich@utexas.edu>
2021-01-31 17:53:47 -0600

    created infoParagraph

--- client/src/shared/containers/infoParagraph/infoParagraph.tsx
+++ client/src/shared/containers/infoParagraph/infoParagraph.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
-const InfoParagraph = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+interface Props {
+  titleText: string;
+  bodyText: string;
+  tags: string[]; //  egs
 }
+
+const generateTags = (tags: string[]) => {
+  let egs: string = '';
+  tags.map((tag: string) => {
+    egs += `• ${tag} `;
+    return 1;
+  });
+  return <b>{egs}</b>;
+};
+
+const InfoParagraph = ({ titleText, bodyText, tags }: Props) => (
+  <div>
+    <h3>{titleText}</h3>
+    <div>{bodyText}</div>
+    {tags && generateTags(tags)}
+  </div>
+);
 
 export default InfoParagraph;
